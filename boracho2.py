48409bc726d3d522bd64aca8507948ac88958b82
Carlosnava16 <carlosalnava4@gmail.com>
2025-05-28 13:03:01 -0700

    primera parte hecha funcion: tomar y usar baño

---
+++ boracho2.py
@@ -0,0 +1,12 @@
+import time
+import random
+
+def tomar(nombre):
+    print(f"{nombre} está tomando cerveza...")
+    time.sleep(1)
+
+def usar_baño(nombre):
+    print(f"{nombre} está en el baño...")
+    time.sleep(1)
+    print(f"{nombre} salió del baño.")
+
