3d3c7312bc835252e28a2daa324dde7ade344c3e
LeslyEJB <lesly.ejb04@gmail.com>
2025-06-07 13:46:24 -0700

    First part of the code made it

---
+++ Drunkers3.py
@@ -0,0 +1,10 @@
+import time
+import random
+
+# Contador de cervezas por borracho
+contador_cervezas = {}
+
+# Inicializar contador
+drunkards = ["Alexis Vega", "Gio Do Santos", "Cristian Calderon", "Marco Fabian", "Carlos Peña"]
+for nombre in drunkards:
+    contador_cervezas[nombre] = 0
